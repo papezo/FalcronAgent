792cefd1f529542c2496c0218e6df110b1245d12
kelqq <papezondrej@seznam.cz>
2025-05-24 21:41:12 +0200

    Update

--- FalcronHook/dllmain.cpp
+++ FalcronHook/dllmain.cpp
@@ -3,14 +3,102 @@
 #include <iostream>
 #include <Minhook.h>
 
-using namespace std;
-
 #if _WIN64
 #pragma comment(lib, "libMinHook.x64.lib")
 #else
 #pragma comment(lib, "libMinHook.x86.lib")
 #endif
 
+typedef DWORD(NTAPI* pNtAllocateVirtualmemory)
+(
+    HANDLE ProcessHandle,
+    PVOID* BaseAddress,
+    ULONG_PTR ZeroBits,
+	PSIZE_T RegionSize,
+	ULONG AllocationType,
+	ULONG Protect
+);
+
+typedef DWORD(WINAPI* pNtProtectVirtualMemory)
+(
+    HANDLE ProcessHandle,
+    PVOID* BaseAddress,
+    PSIZE_T RegionSize,
+    ULONG NewProtect,
+    PULONG OldProtect
+);
+
+pNtAllocateVirtualmemory pOriginalNtAllocateVirtualMemory = nullptr;
+pNtProtectVirtualMemory pOriginalNtProtectVirtualMemory = nullptr;
+
+DWORD NTAPI HookedNtAllocateVirtualMemory(
+    HANDLE ProcessHandle,
+    PVOID* BaseAddress,
+	ULONG_PTR ZeroBits,
+    PSIZE_T RegionSize,
+	ULONG AllocationType,
+	ULONG Protect) {
+
+    if (Protect == PAGE_EXECUTE_READWRITE) {
+        std::cout << "PAGE_EXECUTE_READWRITE permission detected in NtAllocateVirtualMemory function" << std::endl;
+        // if protections enabled then close the process, prevent the call and so on.,.
+    }
+
+    return pOriginalNtAllocateVirtualMemory(
+        ProcessHandle,
+        BaseAddress,
+        ZeroBits,
+        RegionSize,
+        AllocationType,
+        Protect);
+}
+
+DWORD NTAPI HookedNtProtectVirtualMemory(
+    HANDLE ProcessHandle,
+    PVOID* BaseAddress,
+    PSIZE_T RegionSize,
+    ULONG NewProtect,
+    PULONG OldProtect) {
+
+    if (NewProtect == PAGE_EXECUTE_READWRITE) {
+        std::cout << "PAGE_EXECUTE_READWRITE permission detected in NtAllocateVirtualMemory function" << std::endl;
+        // if protections enabled then close the process, prevent the call and so on.,.
+    }
+
+    return pOriginalNtProtectVirtualMemory(
+        ProcessHandle,
+        BaseAddress,
+        RegionSize,
+        NewProtect,
+        OldProtect);
+}
+
+void InitHooks()
+{
+	MH_STATUS status = MH_Initialize();
+
+    if (status != MH_OK) {
+        std::cout << "Failed to initialize MinHook: " << MH_StatusToString(status) << std::endl;
+        return;
+	}
+
+    if (MH_CreateHookApi(L"ntdll", "NtProtectVirtualMemory", &HookedNtProtectVirtualMemory, (LPVOID*)&pOriginalNtAllocateVirtualMemory) != MH_OK)
+    {
+		std::cout << "Failed to create hook NtProtectVirtualMemory: " << MH_StatusToString(status) << std::endl;
+    }
+
+    if (MH_CreateHookApi(L"ntdll", "HookedNtAllocateVirtualMemory", &HookedNtAllocateVirtualMemory, (LPVOID*)&pOriginalNtAllocateVirtualMemory) != MH_OK)
+    {
+        std::cout << "Failed to create hook NtProtectVirtualMemory: " << MH_StatusToString(status) << std::endl;
+    }
+
+    if (MH_EnableHook(MH_ALL_HOOKS) != MH_OK)
+    {
+        std::cout << "Failed to enable hooks: " << std::endl;
+    }
+
+	std::cout << "Hooks initialized successfully." << std::endl;
+}
 void CreateConsole()
 {
     FreeConsole();
@@ -22,12 +110,13 @@ void CreateConsole()
         freopen_s(&file, "CONOUT$", "w", stderr);
         freopen_s(&file, "CONOUT$", "w", stdin);
 
-		cout << "Console has been allocated.." << endl;
+		std::cout << "Console has been allocated.." << std::endl;
     }
 }
 DWORD MainFunction(LPVOID lpParam)
 {
     CreateConsole();
+    InitHooks();
     Sleep(500000);
 
     return 0;
